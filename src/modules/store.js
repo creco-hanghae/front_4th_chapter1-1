7100d8fbb28a68abccac32b64adfc89283adf4fe
divopsor <divopsor@gmail.com>
2024-12-22 20:21:39 +0900

    fix: reviewed 내용 - deepEqual 로 비교한 값이 같으면 bypass

--- src/modules/store.js
+++ src/modules/store.js
@@ -1,14 +1,24 @@
 import { withObserver } from "../hof/withObserver";
+import deepEqual from "deep-equal";
 
 export const store = withObserver({
   get: (key) => {
     return JSON.parse(localStorage.getItem(key));
   },
   set: (key, value) => {
+    const prev = localStorage.getItem(key, JSON.stringify(value));
+    if (deepEqual(prev, value)) {
+      return;
+    }
+
     localStorage.setItem(key, JSON.stringify(value));
     store.notify();
   },
   remove: (key) => {
+    if (localStorage.getItem(key) == null) {
+      return;
+    }
+
     localStorage.removeItem(key);
     store.notify();
   },
